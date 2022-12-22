50f9edb474b9385b475e78c7270d6258ed3ecba3
abbas <abbas@abbass-MacBook-Air.local>
2022-12-22 22:48:55 +0530

    QA1

---
+++ todo-app/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+//import Todoform from './Components/Todoform';
+import Todolist from './Components/Todolist';
+
+function App() {
+  return (
+    <div className='todo-app'>
+        <Todolist/>
+    </div>
+  );
+}
+
+export default App;
+
